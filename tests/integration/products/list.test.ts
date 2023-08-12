e81c23bb711a635a1d79643ec014ca879dbf86fc
nataliaschmidt <nataliak.schmidt@gmail.com>
2023-08-12 14:56:08 -0300

    Feat: Testes de integração para a rota GET /products

--- tests/integration/products/list.test.ts
+++ tests/integration/products/list.test.ts
@@ -1,10 +1,22 @@
 import sinon from 'sinon';
 import chai, { expect } from 'chai';
 import chaiHttp from 'chai-http';
+import ProductModel from '../../../src/database/models/product.model';
+import productsMock from '../../mocks/products.mock';
+import app from '../../../src/app';
 
 chai.use(chaiHttp);
 
 describe('GET /products', function () { 
   beforeEach(function () { sinon.restore(); });
+it('ao fazer uma requisição retorna a lista de produtos', async () => {
+  sinon.stub(ProductModel, 'findAll').resolves()
 
+
+const httpResponse = await chai
+      .request(app)
+      .get('/products')
+
+      expect(httpResponse.status).to.equal(200);
+    })
 });
